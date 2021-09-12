1c7124c99e1056fb43b29545a1d04b4628bdca02
unknown <cheat.book20@gmail.com>
2021-09-12 13:56:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HashFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Enter the string message:");
+                string message = Console.ReadLine();
+                Console.WriteLine("MD-5 Hash: " + MD.MD5(message));
+                Console.WriteLine("SHA-256 Hash: " + SHA.SHA256(message));
+            }
+        }
+    }
+}
